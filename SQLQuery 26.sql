391b951da49698eae632813e2c216a3d50e72356
Kuhle03 <ST10496066@rcconnect.edu.za>
2026-09-03 22:53:56 +0200

    Update SQLQuery 26.sql

--- SQLQuery 26.sql
+++ SQLQuery 26.sql
@@ -3,3 +3,17 @@ VALUES
     ('Anzatshilidzi Nemafukani', 'Anza@raceday.co.za', 'hashed_pw_001', '0628234567'),
     ('Khanyisile Mnguni', 'khanyi@raceday.co.za', 'hashed_pw_002', '0756784321');
 GO
+
+INSERT INTO Events (OrganiserID, Name, EventDate, Location, Description, RouteInfo)
+VALUES
+    (1, 'Durban Park Run', '2026-10-17', 'Durban',
+        'A community park run through the Durban.',
+        'Flat, tarred loop, 5km and 10km options.'),
+    (1, 'Soweto Heritage Cycle Challenge', '2026-10-03', 'Soweto',
+        'A charity cycling event supporting local youth sports programmes.',
+        'Rolling hills, mixed tar and gravel, water points every 15km.'),
+    (2, 'Pretoria Winter Road Race', '2026-11-20', 'Pretoria',
+        'An annual road running event with categories for all experience levels.',
+        'Point-to-point road route, closed to traffic.');
+GO
+
