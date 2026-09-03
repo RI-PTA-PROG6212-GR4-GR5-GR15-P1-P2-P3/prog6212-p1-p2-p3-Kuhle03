ff67af17be40226852b57979c50e22bed91c01a8
Kuhle03 <ST10496066@rcconnect.edu.za>
2026-09-03 22:30:02 +0200

    Update SQLQuery 25.sql

--- SQLQuery 25.sql
+++ SQLQuery 25.sql
@@ -8,6 +8,21 @@ CREATE TABLE Organisers (
 );
 GO
 
+CREATE TABLE Events (
+    EventID         INT IDENTITY(1,1) PRIMARY KEY,
+    OrganiserID     INT             NOT NULL,
+    Name            NVARCHAR(150)   NOT NULL,
+    EventDate       DATE            NOT NULL,
+    Location        NVARCHAR(150)   NOT NULL,
+    Description     NVARCHAR(MAX)   NULL,
+    RouteInfo       NVARCHAR(MAX)   NULL,
+    CONSTRAINT FK_Events_Organisers FOREIGN KEY (OrganiserID)
+        REFERENCES Organisers(OrganiserID)
+);
+GO
+
+
+
 
 
 
